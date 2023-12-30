bc13858da9c074643c1d0d7ecf4c8e429d2e0847
kizwolak <ki.zwolak@gmail.com>
2023-12-30 01:47:34 +0100

    Implement CreatePrizeForm validation

--- TrackerUI/CreatePrizeForm.cs
+++ TrackerUI/CreatePrizeForm.cs
@@ -17,6 +17,50 @@ namespace TrackerUI
             InitializeComponent();
         }
 
+        private bool ValidateForm()
+        {
+            bool output = true;
+            int placeNumber = 0;
+
+            bool placeNumberValidNumber = int.TryParse(PlaceNumberInput.Text, out placeNumber);
+
+            if (!placeNumberValidNumber == false)
+            {
+                output = false;
+            }
+            if (placeNumber < 1) 
+            {
+                output = false;
+            }
+            if (PlaceNumberInput.Text.Length == 0)
+            {
+                output = false;
+            }
+
+            decimal prizeAmount = 0;
+            int prizePercentage = 0;
+
+            bool prizeAmountValid = decimal.TryParse(PrizeAmountInput.Text, out prizeAmount);
+            bool prizePercentageValid = int.TryParse(PrizeAmountInput.Text, out prizePercentage);
+
+            if (prizeAmountValid == false || prizePercentageValid == false)
+            {
+                output = false;
+            }
+
+            if (prizeAmount <= 0 || prizePercentage <= 0)
+            {
+                output = false;
+            }
+
+            if (prizePercentage < 0 || prizePercentage > 100)
+            {
+                output = false;
+            }
+
+            return output;
+        }
+
         private void CreatePrizeForm_Load(object sender, EventArgs e)
         {
 
