3e627e4716a3f1e0c0cc72d39b206628c9944d54
kizwolak <ki.zwolak@gmail.com>
2023-12-31 07:13:52 +0100

    Describe PrizeModel props

--- TrackerLibrary/Models/PrizeModel.cs
+++ TrackerLibrary/Models/PrizeModel.cs
@@ -8,12 +8,30 @@ namespace TrackerLibrary.Models
 {
     public class PrizeModel
     {
-        /* pamietaj, zeby stworzyc jeszcze raz dbo.Prizes!! */
-
+        
+        /// <summary>
+        /// The unique id number of a prize.
+        /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// A prize's place number.
+        /// </summary>
         public int PlaceNumber { get; set; }
+
+        /// <summary>
+        /// A prize's place name.
+        /// </summary>
         public string PlaceName { get; set; }
+
+        /// <summary>
+        /// The amount of a prize.
+        /// </summary>
         public decimal PrizeAmount { get; set; }
+
+        /// <summary>
+        /// The percentage of a prize.
+        /// </summary>
         public double PrizePercentage { get; set; }
 
         public PrizeModel()
